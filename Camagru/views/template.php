fd015d4c6406fce6413564593844fbbb25a22d5a
fherbine <felix.herbinet@yahoo.com>
2018-04-12 12:12:44 +0200

    Adding template for website

---
+++ Camagru/views/template.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="../public/css/style.css" />
+	<title><?= $title ?></title>
+</head>
+<body>
+	<?= $content ?>
+</body>
+</html>
