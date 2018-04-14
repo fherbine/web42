af617a7484b74366767d40457bdf84a71ed72b51
fherbine <felix.herbinet@yahoo.com>
2018-04-14 18:14:44 +0200

    Updating index

--- Camagru/views/indexView.php
+++ Camagru/views/indexView.php
@@ -4,10 +4,12 @@ $title = "photobooth42 - Index";
 
 <?php ob_start(); ?>
 	<link rel="stylesheet" type="text/css" href="public/css/index.css"/>
-	
 <?php $ext_head = ob_get_clean(); ?>
 
+<!-- Main view -->
+
 <?php ob_start(); ?>
+
 	<div class = "begin_drop"><a href="#">Begin the experience</a>
 		<div class = "begin_hide">
 			<p>tutu<p>
@@ -16,12 +18,8 @@ $title = "photobooth42 - Index";
 		</div>
 	</div>
 
-<video id="screenshot-video" autoplay></video>
-<img src="" id="screenshot-img">
-<canvas style="display:none;"></canvas>
-<button id="screenshot-button">tutu</button>
-
-<script type="text/javascript" src="public/scripts/usrCam.js"></script>
 <?php $content = ob_get_clean(); ?>
+
+
 
 <?php require_once('template.php'); ?>
