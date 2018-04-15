4f77d54f7ef7ce8cce69e17b0d00595cec410636
fherbine <felix.herbinet@yahoo.com>
2018-04-15 21:30:16 +0200

    Update

--- Camagru/models/LogManager.php
+++ Camagru/models/LogManager.php
@@ -21,6 +21,7 @@ class LogManager
 		$exist->execute(array($email));
 		if (!($exist->fetch()))
 		{
+			$confirm_key = hash('md5', rand());
 			$request = $db->prepare('INSERT INTO passwd(email, passwd, pseudo, sign_date, admin, sumup, confirm, confirm_key)
 				VALUES(:email, :password, :pseudo, NOW(), :admin, :sumup, :confirm, :confirm_key)');
 			$request->execute(array('email' => $email,
@@ -29,12 +30,35 @@ class LogManager
 								'admin' => $admin,
 								'sumup' => 'Hey I\'m new on Photobooth 42 !',
 								'confirm' => 0,
-								'confirm_key' => 22222
+								'confirm_key' => $confirm_key
 							)); ///////////////////////// -------------------------------- rand -> conf key
-			return 'ok';
+			$url = 'localhost:8080/web42/Camagru/index.php?page=activate&amplogin=' . urlencode($pseudo) . 'key=' . urlencode($confirm_key); ///////////////////////////// ----------------------------------------
+			$content = 'Thanks for your subscription ' . $pseudo . ' and welcome on board.
+			 Please click on the following link to activate your account: ' . $url;
+			$content = wordwrap($content, 70, "\r\n");
+			$this->sendMail('felix.herbinet@yahoo.com', $email, 'Activation of your account', $content);
+			return "ok";
 		}
 		else
 			return "usr already exist"; ///// -------------------------------------------
+	}
+
+	public function sendMail($from, $to, $subject, $content)
+	{
+		$email_header = 'From: ' . $from . "\r\n" .
+ 		'X-Mailer: PHP/' . phpversion();
+ 		try
+ 		{
+			mail($to, $subject, $content, $email_header);
+		}
+		catch(Exception $e)
+		{
+			die('An error occured: ' . $e->getMessage());
+		}
+		var_dump($tst);
+		echo $to . "<br/>";
+		echo $content . "<br/>";
+		echo $email_header . "<br/>";
 	}
 
 	private function dbConnect()
