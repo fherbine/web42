a5f6bf28ea82983eb19e3964668f89d80ee2df31
fherbine <felix.herbinet@yahoo.com>
2018-04-18 12:18:20 +0200

    updating script for webcam

--- Camagru/public/scripts/usrCam.js
+++ Camagru/public/scripts/usrCam.js
@@ -10,29 +10,45 @@ const video = document.querySelector('#screenshot-video');
 const canvas = document.createElement('canvas');
 
 var select, getImg;
-const width = 150;
+const width = 2800;
 var height = 0;
 
-button.onclick = video.onclick = function() {
-	canvas.width = width;
-	height = video.videoHeight / (video.videoWidth/width);
-	canvas.height = height;
-	ctx = canvas.getContext('2d');
-	ctx.drawImage(video, 0, 0, width, height);
-	img.src = canvas.toDataURL('image/webp');
-	getImg = "getImg=" + img.src.split(',')[1];
-	document.cookie = getImg;
-	select = '<a href ="index.php?action=upload_img" >KEEP THIS</a>';
-	console.log(img.src.length); //////////////////// debug size
-	console.log(btoa(ctx.getImageData(10,10,50,50).data).length); //////////////////////// debug size
-	document.querySelector('#buttons').innerHTML += select;
-};
+button.addEventListener("click", function()
+	{
+		canvas.width = width;
+		height = video.videoHeight / (video.videoWidth/width);
+		canvas.height = height;
+		ctx = canvas.getContext('2d');
+		ctx.drawImage(video, 0, 0, width, height);
+		img.src = canvas.toDataURL('image/webp');
+		if (!document.getElementById('keep-it'))
+		{
+			select = '<button id="keep-it" >KEEP THIS</a>';
+			document.getElementById('buttons').innerHTML += select;
+		}
+		console.log(img.src.length); //////////////////// debug size
+		console.log(btoa(ctx.getImageData(10,10,50,50).data).length); //////////////////////// debug size
+		keepit();
+	}
+);
+
+function keepit()
+{
+	var keep = document.getElementById('keep-it');
+
+	keep.addEventListener("click", function()
+		{
+			console.log("toto");
+		}
+	);
+}
+
 
 function handleSuccess(stream) {
-video.srcObject = stream;
+	video.srcObject = stream;
 }
 function handleError(error) {
-console.error('Reeeejected!', error);
+	console.error('Reeeejected!', error);
 }
 
 navigator.mediaDevices.getUserMedia(constraints).
